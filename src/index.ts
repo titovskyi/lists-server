771cbf26719d8c3db87a40d7b81efe6cbb087fa5
titovskyi <titovskyi@gmail.com>
2021-02-27 14:07:56 +0200

    process.env.PORT

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ createConnection()
 
         app.use('/', routes);
 
-        app.listen(3000, () => {
+        app.listen(process.env.PORT || 3000, () => {
             console.log('Server started on port 3000!');
         });
     })
