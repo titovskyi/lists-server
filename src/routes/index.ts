f8b0035beeadc1eba0d2e38ae00a5c7fe2a25296
titovskyi <titovskyi@gmail.com>
2021-02-27 13:54:03 +0200

    add orm url

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -11,3 +11,10 @@ routes.use('/list', [checkJwt], list);
 routes.use('/product', [checkJwt], product);
 
 export default routes;
+
+
+//   "host": "localhost",
+//   "port": 5432,
+//   "username": "postgres",
+//   "password": "0123456789",
+//   "database": "purchaseLists",
